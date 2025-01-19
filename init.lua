9b9027c27f027b509a1067f3333568572aea5346
Mehul-Kumar-27 <mehul2002kumar@gmail.com>
2025-01-20 00:06:27 +0530

    final edits

--- init.lua
+++ init.lua
@@ -18,4 +18,29 @@ vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
 
+
+-- Define a function for circular navigation
+local function circular_window_nav(direction)
+    local cur_win = vim.fn.winnr()
+    local max_win = vim.fn.winnr('$') -- Get the total number of windows
+
+    if direction == "right" then
+        if cur_win == max_win then
+            vim.cmd("wincmd t") -- Go to the first window if at the last
+        else
+            vim.cmd("wincmd w") -- Go to the next window
+        end
+    elseif direction == "left" then
+        if cur_win == 1 then
+            vim.cmd("wincmd W") -- Go to the last window if at the first
+        else
+            vim.cmd("wincmd W") -- Go to the previous window
+        end
+    end
+end
+
+-- Map keys for circular navigation
+vim.keymap.set("n", "<Space>l", function() circular_window_nav("right") end, { desc = "Go to the next window (circular)" })
+vim.keymap.set("n", "<Space>h", function() circular_window_nav("left") end, { desc = "Go to the previous window (circular)" })
+
 require("lazy").setup("plugins")
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" },
+				ensure_installed = { "lua_ls", "clangd" },
 			})
 		end,
 	},
@@ -21,6 +21,9 @@ return {
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
